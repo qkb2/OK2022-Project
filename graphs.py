a2fe16af0d27b7c8cb3aa4342981b05b53164572
qkb2 <53836634+qkb2@users.noreply.github.com>
2022-10-16 12:08:47 +0200

    Added greedy coloring - must be tested.

--- graphs.py
+++ graphs.py
@@ -1,7 +1,7 @@
 class Vertex:
     def __init__(self, name) -> None:
         self.name = name
-        self.color = 0
+        self.color = -1
         self.is_visited = 0
 
 class Graph:
@@ -53,8 +53,36 @@ class Graph:
         self.create_edge_list_from_file(faddr)
         self.create_matrix_from_edge_list()
 
+    def greedy_coloring(self) -> None:
+        for i in range(self.V):
+            colors = [0 for _ in range(G.V)]
+            for j in range(self.V):
+                if self.matrix[i][j] == 1:
+                    if self.vertices[j].color == -1:
+                        continue
+                    colors[self.vertices[j].color] = 1
+            for j in range(self.V):
+                if colors[j] == 0:
+                    self.vertices[i].color = j
+                    break
+
+    def get_greatest_color(self) -> int:
+        c = 0
+        for v in self.vertices:
+            if v.color > c:
+                c = v.color
+        return c
+
+    def get_color_str(self) -> str:
+        s = ''
+        for v in self.vertices:
+            s += str(v.name) + ' has color ' + str(v.color) + '\n'
+        return s
 
 if __name__ == '__main__':
     G = Graph()
     G.create_graph()
     print(G)
+    G.greedy_coloring()
+    print(G.get_greatest_color())
+    print(G.get_color_str())
